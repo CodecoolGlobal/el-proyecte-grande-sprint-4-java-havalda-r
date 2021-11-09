2ebc0940523636dd9d4272b17d11e1831c2e3964
Richard Havalda <havalda.r94@gmail.com>
2021-11-09 12:54:32 +0100

    feat: initialize routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Layout>
-      <h1>MAIN</h1>
+      <Routes>
+        <Route
+          path='/menu'
+          exact
+          element={
+            <div>
+              <h1>Menu page</h1>
+            </div>
+          }
+        />
+        <Route
+          path='/menu'
+          exact
+          element={
+            <div>
+              <h1>Promotions page</h1>
+            </div>
+          }
+        />
+        <Route
+          path='/'
+          exact
+          element={
+            <div>
+              <h1>Home page</h1>
+            </div>
+          }
+        />
+      </Routes>
     </Layout>
   );
 }
