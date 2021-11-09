eb875c3b6ddecc5bbb40078506922d086cffeda7
Richard Havalda <havalda.r94@gmail.com>
2021-11-09 14:56:54 +0100

    feat: add background

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import styles from './Layout.module.css';
 
 const Layout = (props) => {
   return (
-    <>
+    <div className={styles.Layout}>
       <Header />
       <main>{props.children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
