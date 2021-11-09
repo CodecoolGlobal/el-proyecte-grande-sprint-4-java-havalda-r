1f285d4860e1f49b3dea83941bfc8933169f3981
Richard Havalda <havalda.r94@gmail.com>
2021-11-09 15:04:27 +0100

    feat: initialize header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.Header}>
       <Link to='/'>
         <div>LOGO</div>
       </Link>
