fb9ab010a747b531716f72d18c9d3bf1a13e8196
Richard Havalda <havalda.r94@gmail.com>
2021-11-09 12:39:54 +0100

    feat: add placeholders to footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        &copy; 2021 JavaPeno | All rights reserved | Terms of Service | Privacy
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
