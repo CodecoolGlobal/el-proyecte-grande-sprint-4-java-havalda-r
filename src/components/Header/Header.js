3f70e7fbb2a1755228d00baea53a17fd8e386358
Richard Havalda <havalda.r94@gmail.com>
2021-11-09 12:37:42 +0100

    feat: add placeholders to header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>LOGO</div>
+      <div>MENU</div>
+      <div>PROMOTIONS</div>
+      <div>LOGIN</div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Layout = (props) => {
   return (
     <>
-      <header>HEADER</header>
+      <Header />
       <main>{props.children}</main>
       <footer>FOOTER</footer>
     </>
