696367cbb6e8dd899b8ca54676fee97d82fa06e4
Richard Havalda <havalda.r94@gmail.com>
2021-11-10 09:51:21 +0100

    feat: align header items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,24 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <header className={styles.Header}>
-      <Link to='/'>
-        <div>LOGO</div>
-      </Link>
-      <Link to='/menu'>
-        <div>MENU</div>
-      </Link>
-      <Link to='/promotions'>
-        <div>PROMOTIONS</div>
-      </Link>
-      <Link to='/builder'>
-        <div>MAKE IT JAVA</div>
-      </Link>
-      <div>LOGIN</div>
+      <div className={[styles.container, styles.left].join(' ')}>
+        <Link to='/'>
+          <div>LOGO</div>
+        </Link>
+        <Link to='/menu'>
+          <div>MENU</div>
+        </Link>
+        <Link to='/promotions'>
+          <div>PROMOTIONS</div>
+        </Link>
+        <Link to='/builder'>
+          <div>MAKE IT JAVA</div>
+        </Link>
+      </div>
+      <div className={[styles.container, styles.right].join(' ')}>
+        <div>CART</div>
+        <div>LOGIN</div>
+      </div>
     </header>
   );
 };
