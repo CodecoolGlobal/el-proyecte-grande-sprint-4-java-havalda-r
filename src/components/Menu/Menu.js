eb9bbea3e0485ca1ff78e052089d5dbd8429c764
Richard Havalda <havalda.r94@gmail.com>
2021-11-10 11:05:07 +0100

    feat: initialize container style

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Menu.module.css';
 
 const Menu = () => {
   return (
-    <div>
-      <ul>
-        <Link to='/menu/meals'>MEALS</Link>
-        <Link to='/menu/special-offers'>SPECIAL OFFERS</Link>
-        <Link to='/menu/beverages'>BEVERAGES</Link>
-        <Link to='/menu/sandwiches'>SANDWICHES</Link>
-        <Link to='/menu/side-dishes'>SIDE DISHES</Link>
-        <Link to='/menu/salads'>SALADS</Link>
-        <Link to='/menu/accessories'>ACCESSORIES</Link>
-      </ul>
+    <div className={styles.Menu}>
+      <Link to='/menu/meals'>MEALS</Link>
+      <Link to='/menu/special-offers'>SPECIAL OFFERS</Link>
+      <Link to='/menu/beverages'>BEVERAGES</Link>
+      <Link to='/menu/sandwiches'>SANDWICHES</Link>
+      <Link to='/menu/side-dishes'>SIDE DISHES</Link>
+      <Link to='/menu/salads'>SALADS</Link>
+      <Link to='/menu/accessories'>ACCESSORIES</Link>
     </div>
   );
 };
