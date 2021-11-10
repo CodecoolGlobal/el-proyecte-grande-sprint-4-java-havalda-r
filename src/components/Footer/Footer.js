6bca49466aade80b8b80b4141b74607972bff472
Richard Havalda <havalda.r94@gmail.com>
2021-11-10 09:33:54 +0100

    feat: add footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.Footer}>
       <p>
         &copy; 2021 JavaPeno | All rights reserved | Terms of Service | Privacy
       </p>
