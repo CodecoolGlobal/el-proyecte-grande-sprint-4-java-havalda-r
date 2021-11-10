10db7a48f7f71ca51478da0ad9d1b74e4db128f9
Richard Havalda <havalda.r94@gmail.com>
2021-11-10 12:11:12 +0100

    feat: add card style

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Menu.module.css';
+import MenuImg from '../../assets/images/menu.jpg';
+import SpecialOfferImg from '../../assets/images/special_offer.jpg';
+import BeveragesImg from '../../assets/images/beverages.jpg';
+import SandwichesImg from '../../assets/images/sandwiches.jpg';
+import SideDishImg from '../../assets/images/side_dish.jpg';
+import SaladImg from '../../assets/images/salad.jpg';
+import SaucesImg from '../../assets/images/sauces.jpg';
+
+import OtherImg from '../../assets/images/other.jpg';
 
 const Menu = () => {
   return (
     <div className={styles.Menu}>
-      <Link to='/menu/meals'>MEALS</Link>
-      <Link to='/menu/special-offers'>SPECIAL OFFERS</Link>
-      <Link to='/menu/beverages'>BEVERAGES</Link>
-      <Link to='/menu/sandwiches'>SANDWICHES</Link>
-      <Link to='/menu/side-dishes'>SIDE DISHES</Link>
-      <Link to='/menu/salads'>SALADS</Link>
-      <Link to='/menu/accessories'>ACCESSORIES</Link>
+      <div>
+        <h1>MENU</h1>
+      </div>
+      <div className={styles.cardContainer}>
+        <Link to='/menu/meals' className={styles.link}>
+          <div className={styles.card}>
+            <img src={MenuImg} />
+            <h2>Meals</h2>
+          </div>
+        </Link>
+        <Link to='/menu/special-offers' className={styles.link}>
+          <div className={styles.card}>
+            <img src={SpecialOfferImg} />
+            <h2>Special offers</h2>
+          </div>
+        </Link>
+        <Link to='/menu/beverages' className={styles.link}>
+          <div className={styles.card}>
+            <img src={BeveragesImg} />
+            <h2>Beverages</h2>
+          </div>
+        </Link>
+        <Link to='/menu/sandwiches' className={styles.link}>
+          <div className={styles.card}>
+            <img src={SandwichesImg} />
+            <h2>Sandwiches</h2>
+          </div>
+        </Link>
+        <div className={styles.break} />
+        <Link to='/menu/side-dishes' className={styles.link}>
+          <div className={styles.card}>
+            <img src={SideDishImg} />
+            <h2>Side dishes</h2>
+          </div>
+        </Link>
+        <Link to='/menu/salads' className={styles.link}>
+          <div className={styles.card}>
+            <img src={SaladImg} />
+            <h2>Salads</h2>
+          </div>
+        </Link>
+        <Link to='/menu/accessories' className={styles.link}>
+          <div className={styles.card}>
+            <img src={SaucesImg} />
+            <h2>Accessories</h2>
+          </div>
+        </Link>
+        <Link to='/menu/accessories' className={styles.link}>
+          <div className={styles.card}>
+            <img src={OtherImg} />
+            <h2>Other</h2>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
