26a6f011ead845570376cbcf48b2a3cba989a2bb
Richard Havalda <havalda.r94@gmail.com>
2021-11-25 00:43:39 +0100

    feat: add conditional render

--- src/components/CartCard/CartCard.js
+++ src/components/CartCard/CartCard.js
@@ -6,12 +6,22 @@ const CartCard = (props) => {
     <div className={styles.CartCard}>
       <h2>A sandwich with:</h2>
       <ul>
-        <li>{props.sw.ingredients.jalapeno} portion of extra spicy jalapeno</li>
-        <li>{props.sw.ingredients.salad} chunks of fresh salad</li>
-        <li>{props.sw.ingredients.cheese} slices of cheddar</li>
-        <li>
-          {props.sw.ingredients.meat} cuts of the finest angus beef hamburger
-        </li>
+        {props.sw.ingredients.jalapeno > 0 ? (
+          <li>
+            {props.sw.ingredients.jalapeno} portion of extra spicy jalapeno
+          </li>
+        ) : null}
+        {props.sw.ingredients.salad > 0 ? (
+          <li>{props.sw.ingredients.salad} chunks of fresh salad</li>
+        ) : null}
+        {props.sw.ingredients.cheese > 0 ? (
+          <li>{props.sw.ingredients.cheese} slices of cheddar</li>
+        ) : null}
+        {props.sw.ingredients.meat > 0 ? (
+          <li>
+            {props.sw.ingredients.meat} cuts of the finest angus beef hamburger
+          </li>
+        ) : null}
       </ul>
       <h3>For the price of {props.sw.totalPrice} HUF</h3>
     </div>
