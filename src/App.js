f52537ce6f4f05af25e38be76b60a60d52d1e962
Richard Havalda <havalda.r94@gmail.com>
2021-11-24 14:25:02 +0100

    feat: add context

--- src/App.js
+++ src/App.js
@@ -6,38 +6,50 @@ import Menu from './components/Menu/Menu';
 import Home from './components/Home/Home';
 import Cart from './components/Cart/Cart';
 import Builder from './components/Builder/Builder';
+import { CartContext } from './components/Cart/CartContext';
 
 function App() {
-  const [cart, setCart] = useState(null);
+  const [cart, setCart] = useState([]);
 
   return (
     <Layout>
-      <Routes>
-        <Route path='/builder' exact element={<Builder />} />
-        <Route path='/menu' exact element={<Menu />} />
-        <Route path='/menu/meals' exact element={<h1>MEALS</h1>} />
-        <Route
-          path='/menu/special-offers'
-          exact
-          element={<h1>SPECIAL OFFERS</h1>}
-        />
-        <Route path='/menu/beverages' exact element={<h1>BEVERAGES</h1>} />
-        <Route path='/menu/sandwiches' exact element={<h1>SANDWICHES</h1>} />
-        <Route path='/menu/side-dishes' exact element={<h1>SIDE-DISHES</h1>} />
-        <Route path='/menu/salads' exact element={<h1>SALADS</h1>} />
-        <Route path='/menu/accessories' exact element={<h1>ACCESSORIES</h1>} />
-        <Route path='/cart' exact element={<Cart cart={cart} />} />
-        <Route
-          path='/promotions'
-          exact
-          element={
-            <div>
-              <h1>Promotions page</h1>
-            </div>
-          }
-        />
-        <Route path='/' exact element={<Home />} />
-      </Routes>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <Routes>
+          <Route path='/builder' exact element={<Builder cart={cart} />} />
+          <Route path='/cart' exact element={<Cart cart={cart} />} />
+
+          <Route path='/menu' exact element={<Menu />} />
+          <Route path='/menu/meals' exact element={<h1>MEALS</h1>} />
+          <Route
+            path='/menu/special-offers'
+            exact
+            element={<h1>SPECIAL OFFERS</h1>}
+          />
+          <Route path='/menu/beverages' exact element={<h1>BEVERAGES</h1>} />
+          <Route path='/menu/sandwiches' exact element={<h1>SANDWICHES</h1>} />
+          <Route
+            path='/menu/side-dishes'
+            exact
+            element={<h1>SIDE-DISHES</h1>}
+          />
+          <Route path='/menu/salads' exact element={<h1>SALADS</h1>} />
+          <Route
+            path='/menu/accessories'
+            exact
+            element={<h1>ACCESSORIES</h1>}
+          />
+          <Route
+            path='/promotions'
+            exact
+            element={
+              <div>
+                <h1>Promotions page</h1>
+              </div>
+            }
+          />
+          <Route path='/' exact element={<Home />} />
+        </Routes>
+      </CartContext.Provider>
     </Layout>
   );
 }
