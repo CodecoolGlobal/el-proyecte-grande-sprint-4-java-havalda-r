ddb5c2a7924605a2680fc61319433f3f9b683044
Richard Havalda <havalda.r94@gmail.com>
2021-11-24 20:59:19 +0100

    feat: add total price to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,9 +5,18 @@ import CartCard from '../CartCard/CartCard';
 
 const Cart = (props) => {
   const { cart, setCart } = useContext(CartContext);
+
   const cartItems = cart.map((sandwich, index) => (
     <CartCard key={index} sw={sandwich}></CartCard>
   ));
+
+  const sumTotalPrices = cart.reduce(
+    (sum, listItem) => sum + listItem.totalPrice,
+    0
+  );
+
+  const sandwichCount = cart.length;
+
   console.log(cart);
   return (
     <div className={styles.Cart}>
@@ -15,6 +24,11 @@ const Cart = (props) => {
         <div>
           <h1>You are going to order:</h1>
           <div className={styles.cardContainer}>{cartItems}</div>
+          <p>
+            You are going to buy <span>{sandwichCount}</span> sandwiches for
+            just only
+            <span> {sumTotalPrices}</span> HUF!
+          </p>
         </div>
       ) : (
         <h1>Your cart is empty!</h1>
